afbd8e648fa8351b8d61597e9dea1541bfb18991
Achugwo Michael <139189514+MichaelAchugwo@users.noreply.github.com>
2024-10-29 00:40:28 +0100

    finished contact us page

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,16 +1,25 @@
-import styles from "../css/Hero.module.css";
+import Link from "next/link";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import ContactPhoneIcon from "@mui/icons-material/ContactPhone";
 
 export default function Home() {
   return (
     <>
-      <video autoPlay muted loop className={styles.backgroundVideo}>
-        <source src="/videos/backgroundVideo.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-      <div className={`${styles.overlay}`}>
-        <p className="text-3xl mt-[1000px]">
-          
-        </p>
+      <div className="md:text-center text-2xl z-1 relative px-[50px] mt-10 flex place-items-center justify-center min-h-[65dvh]">
+        <div className="flex text-center flex-col gap-y-5 mt-5">
+          <p>
+            We&apos;d love to hear from you! Whether you have questions,
+            feedback, or need support with Quizoor, feel free to get in touch.
+          </p>
+          <Link href="mailto:mail.quizoor.com">
+            <MailOutlineIcon className="mr-2" />
+            mail.quizoor.com
+          </Link>
+          <Link href="tel:+2348135043731">
+            <ContactPhoneIcon className="mr-2" />
+            +234 813 504 3731
+          </Link>
+        </div>
       </div>
     </>
   );
