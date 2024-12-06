abba6a268fa89f5ae3812971872eed922337fdab
Achugwo Michael <139189514+MichaelAchugwo@users.noreply.github.com>
2024-12-06 02:15:48 +0100

    removed unused function

--- app/(quiz)/quiz/[id]/page.tsx
+++ app/(quiz)/quiz/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Suspense, useEffect, useState } from "react";
 import Loader from "../../extras/Loader";
-import { getQuiz, connectToDB } from "@/app/lib/actions";
+import { getQuiz } from "@/app/lib/actions";
 
 type Question = {
   question: string;
